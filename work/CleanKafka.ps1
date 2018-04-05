5b4b30c055d61df8321021124c2121fde5c68ef3
DRypa <dr.drypa.k@mail.ru>
2018-04-05 16:21:03 +0300

    recreate container

--- work/CleanKafka.ps1
+++ work/CleanKafka.ps1
@@ -1,3 +1,6 @@
+$env:COMPOSE_CONVERT_WINDOWS_PATHS = 1;
+
 & docker-compose stop
-Remove-Item -Path C:\kafka-log\* -Force -Recurse
+& docker-compose rm -f
+Remove-Item -Path C:\kafka-log -Force -Recurse
 & docker-compose up -d
